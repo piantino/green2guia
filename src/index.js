1d581cd4df010c7053962d5ebc1c5c39072fef74
Andre Piantino <piantino@gmail.com>
2020-01-14 16:06:21 -0300

    greencard captcha passed

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
-import crawler from "crawler";
+import Crawler from "crawler";
 
-crawler.direct({
-    uri: "https://www.grupogreencard.com.br/sysweb/site/loginUsuario",
-    callback: function(err, res, done){
-        if(err) {
-            console.log(error);
-        } else {
-            console.log('Grabbed', res.statusCode, res.body.length, 'bytes');
-
-            const captcha = res.$(".eL-captcha strong").text();
-            console.log('Captcha', res.$(".eL-captcha strong").text());
-
-            login(captcha);
-        }
-        done();
-    }
+var crawler = new Crawler({
+    rateLimit: 2000,
+    maxConnections: 1
 });
 
-function login(captchaValue) {
+function iniciar(cpf, pwd) {
+
+    crawler.direct({
+        uri: "https://www.grupogreencard.com.br/sysweb/site/loginUsuario",
+        callback: function(err, res, done){
+            if(err) {
+                console.log(error);
+                return;
+            }
+            console.log('Grabbed', res.statusCode, res.body.length, 'bytes');
+            
+            const captcha = res.$(".eL-captcha strong").text();
+            console.log('Captcha', captcha);
+            
+            const captchaOptions = {};
+            res.$(".eL-possibilities img").each(function(i, e) {
+                const img = res.$(e);
+                captchaOptions[img.attr("alt")] = img.attr("data-value");
+            });
+
+            console.info('Options', captchaOptions);
+
+            const captchaValue = captchaOptions[captcha];
+            console.info('Captcha value', captchaValue);
+
+            login(cpf, pwd, captchaValue);
+        }
+    });
     
-    
+}
+ 
+function login(cpf, pwd, captchaValue) {
+        
+        
     crawler.direct({
         uri: "https://www.grupogreencard.com.br/sysweb/site/loga_usuario",
-        cpf: "045.614.349-43",
-        senha: "6472932",
+        cpf: cpf,
+        senha: pwd,
         'captcha-value': captchaValue,
         callback: function (error, res, done) {
             if(error){
                 console.log(error);
-            }else{
-                console.log('Grabbed', res.statusCode,  res.body.length, 'bytes');
+                return;
             }
-            done();
+            console.log('Grabbed', res.statusCode,  res.body.length, 'bytes');
+            console.log('Error:', res.$("#erro p").text());
         }
     });   
     
 }
+
+iniciar(1, 2);
