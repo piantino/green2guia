7150f9194a30a47cf91df83e06db3eaab741e8f0
Andre Piantino <piantino@gmail.com>
2020-01-17 11:04:15 -0300

    Added guiabolso email and password

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,20 +2,30 @@ var app = new Vue({
   el: '#app',
   data: {
     greencard: {},
-    extract: null,//[{"date":"12/01/2020","value":"R$ 91,63","text":"PASTELARIA"},{"date":"30/12/2019","value":"R$ 90,14","text":"SUPERMERCADOS DO JOAO"},{"date":"21/12/2019","value":"R$ 677,63","text":"SUPERMERCADOS ANGELONI"},{"date":"20/12/2019","value":"R$ 507,10","text":"DISPONIB. BENEFICIO"},{"date":"28/11/2019","value":"R$ 484,05","text":"DISPONIB. BENEFICIO"},{"date":"23/11/2019","value":"R$ 410,76","text":"SUPERMERCADOS PEDRO"},{"date":"28/10/2019","value":"R$ 661,00","text":"DISPONIB. BENEFICIO"}],
-    loginError: false
+    guiabolso: {},
+    extract: null,
+    loginError: null,
   },
   methods: {
     getExtract() {
       this.loginError = false;
 
       axios({ method: "POST", "url": "greencards", "data": this.greencard, "headers": { "content-type": "application/json" } })
-        .then(
-          result => {
-            this.extract = result.data;
-          }, error => {
-            this.loginError = error.response.data;
-          });
+        .then(result => {
+          this.extract = result.data;
+        }, error => {
+          this.loginError = error.response.data;
+        });
+    },
+    sendTransactions() {
+      this.guiabolso.transactions = this.extract;
+      axios({ method: "POST", "url": "guiabolso", "data": this.guiabolso, "headers": { "content-type": "application/json" } })
+        .then(result => {
+          console.log(result);
+        }, error => {
+          console.error(error);
+          this.loginError = error.response.data;
+        });
     }
   }
 });
--- robo/guiabolso.js
+++ robo/guiabolso.js
@@ -6,11 +6,16 @@ const LOGIN_PAGE_URL = "https://www.guiabolso.com.br/web/#/login";
 
 let options = new chrome.Options();
 //Below arguments are critical for Heroku deployment
-options.addArguments("--headless");
+//options.addArguments("--headless");
 //options.addArguments("--disable-gpu");
 //options.addArguments("--no-sandbox");
 
 function addTransactions(email, password, transactions, callback) {
+
+    if (!email || !password) {
+        callback("Informe o login e senha do guiabolso");
+        return;
+    }
 
     console.log("Starting Chrome", options);
     
@@ -25,11 +30,37 @@ function addTransactions(email, password, transactions, callback) {
             await driver.findElement(By.id('email')).sendKeys(email);
             await driver.findElement(By.id('password')).sendKeys(password, Key.RETURN);
             await driver.wait(until.elementLocated(By.xpath('//span[text()="Adicionar transação"]')), 5000);
-            await driver.findElement(By.xpath('//span[text()="Adicionar transação"]')).click();
+            
+            transactions.forEach(t => fillTransaction(driver, t));
+            
+            callback(null, []);
         } finally {
-            await driver.quit();
+            //await driver.quit();
         }
     })();
+}
+
+async function fillTransaction(driver, t) {
+    console.log('Transaction', t);
+    
+    await driver.sleep(1000);
+    
+    await driver.findElement(By.xpath('//span[text()="Adicionar transação"]')).click();
+    await driver.wait(until.elementLocated(By.id('add-transaction-name')), 5000);
+
+    if (t.positive) {
+        await driver.findElement(By.xpath('//input[name="transactionType" and value="income"]')).click();
+    }
+    await driver.findElement(By.id('add-transaction-name')).sendKeys(t.text);
+    await driver.findElement(By.name('transactionValue')).sendKeys(t.value);
+
+    const date = t.date.split("/");
+    await driver.findElement(By.name('day')).sendKeys(date[0]);
+    await driver.findElement(By.name('month')).sendKeys(date[1]);
+    await driver.findElement(By.name('year')).sendKeys(date[2]);
+
+    await driver.findElement(By.xpath('//div[text()="De qual conta é?"]')).click();
+    await driver.findElement(By.xpath('//li[text()="Green Card"]')).click();
 }
 
 module.exports = guiabolso = {
